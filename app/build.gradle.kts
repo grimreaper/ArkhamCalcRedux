b2b970cc94106d182bf6b036d4608a99c2015d8b
Eitan Adler <lists@eitanadler.com>
2025-09-08 22:34:48 -0700

    [fastlane] bump version code to 15

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         applicationId = "com.eitanadler.arkhamcalcredux2"
         minSdk = 26
         targetSdk = 36
-        versionCode = 14
+        versionCode = 15
         versionName = "9.$versionCode"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
