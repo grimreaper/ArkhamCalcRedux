0de184b45e0741f9e64339a3a44793edf578a54f
Eitan Adler <lists@eitanadler.com>
2026-06-14 14:16:02 -0700

    [android] remove deprecated code usage from ExpandableListActivity
    I will eventually want to fully clean this up.
    
    - the expansion code is broken (and it was broken before this too)
    - use more jetpack compose instead of old style
    - remove deprecated code that I only copied

--- app/src/main/java/com/eitanadler/arkhamcalcredux2/ArkhamCalcHelp.kt
+++ app/src/main/java/com/eitanadler/arkhamcalcredux2/ArkhamCalcHelp.kt
@@ -16,8 +16,11 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.*/
 package com.eitanadler.arkhamcalcredux2
 
-import android.app.ExpandableListActivity
+import android.app.Activity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.ContextMenu.ContextMenuInfo
+import android.view.View
 import android.widget.ExpandableListAdapter
 import android.widget.ExpandableListView
 import android.widget.SimpleExpandableListAdapter
@@ -26,8 +29,12 @@ import android.widget.SimpleExpandableListAdapter
  * An activity that displays help topics and content. Strings are stored in the
  * res folder. The nth entry in the 'topic' String array is associated with
  * the nth entry in the 'content' String array.
+ *
+ * Much of this code comes from ExpandableListActivity. Copied to avoid deprecation and then cleaned up to avoid keeping copied generic code. Future work to fix this.
  */
-public class ArkhamCalcHelp : ExpandableListActivity() {
+public class ArkhamCalcHelp : Activity(), View.OnCreateContextMenuListener,
+    ExpandableListView.OnChildClickListener, ExpandableListView.OnGroupCollapseListener,
+    ExpandableListView.OnGroupExpandListener {
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -35,10 +42,15 @@ public class ArkhamCalcHelp : ExpandableListActivity() {
         val contents = getHelp(R.array.contents)
 
         val adapter: ExpandableListAdapter = SimpleExpandableListAdapter(
-            this, getGroupData(topics), R.layout.help_group_row,
-            arrayOf("helpTopic"), intArrayOf(R.id.helpTopicTextView),
-            getChildData(contents), R.layout.help_child_row,
-            arrayOf("helpContent"), intArrayOf(R.id.helpContentTextView)
+            this,
+            getGroupData(topics),
+            R.layout.help_group_row,
+            arrayOf("helpTopic"),
+            intArrayOf(R.id.helpTopicTextView),
+            getChildData(contents),
+            R.layout.help_child_row,
+            arrayOf("helpContent"),
+            intArrayOf(R.id.helpContentTextView)
         )
         setListAdapter(adapter)
 
@@ -63,7 +75,7 @@ public class ArkhamCalcHelp : ExpandableListActivity() {
     internal companion object {
         const val BUNDLE_TOPIC: String = "BUNDLE_TOPIC"
 
-        private fun getGroupData(topics: Iterable<String>): List<Map<String, String>> {
+        internal fun getGroupData(topics: Iterable<String>): List<Map<String, String>> {
             val groupList: MutableList<MutableMap<String, String>> =
                 ArrayList()
 
@@ -92,4 +104,105 @@ public class ArkhamCalcHelp : ExpandableListActivity() {
             return childrenList
         }
     }
+
+    private var mAdapter: ExpandableListAdapter? = null
+    private var mList: ExpandableListView? = null
+    private var mFinishedStart: Boolean = false
+
+    /**
+     * Override this to populate the context menu when an item is long pressed. menuInfo
+     * will contain an [ExpandableListView.ExpandableListContextMenuInfo]
+     * whose packedPosition is a packed position
+     * that should be used with [ExpandableListView.getPackedPositionType] and
+     * the other similar methods.
+     *
+     *
+     * {@inheritDoc}
+     */
+    override fun onCreateContextMenu(menu: ContextMenu?, v: View?, menuInfo: ContextMenuInfo?) {
+    }
+
+    /**
+     * Override this for receiving callbacks when a child has been clicked.
+     *
+     *
+     * {@inheritDoc}
+     */
+    override fun onChildClick(
+        parent: ExpandableListView?, v: View?, groupPosition: Int,
+        childPosition: Int, id: Long
+    ): Boolean {
+        return false
+    }
+
+    /**
+     * Override this for receiving callbacks when a group has been collapsed.
+     */
+    override fun onGroupCollapse(groupPosition: Int) {
+    }
+
+    /**
+     * Override this for receiving callbacks when a group has been expanded.
+     */
+    override fun onGroupExpand(groupPosition: Int) {
+    }
+
+    /**
+     * Ensures the expandable list view has been created before Activity restores all
+     * of the view states.
+     *
+     * @see Activity.onRestoreInstanceState
+     */
+    override fun onRestoreInstanceState(state: Bundle) {
+        ensureList()
+        super.onRestoreInstanceState(state)
+    }
+
+    /**
+     * Updates the screen state (current list and other views) when the
+     * content changes.
+     *
+     * @see Activity.onContentChanged
+     */
+    override fun onContentChanged() {
+        super.onContentChanged()
+        val emptyView = findViewById<View?>(android.R.id.empty)
+        mList = findViewById<View?>(android.R.id.list) as ExpandableListView?
+        if (mList == null) {
+            throw RuntimeException(
+                "Your content must have a ExpandableListView whose id attribute is " +
+                        "'android.R.id.list'"
+            )
+        }
+        if (emptyView != null) {
+            mList!!.setEmptyView(emptyView)
+        }
+        mList!!.setOnChildClickListener(this)
+        mList!!.setOnGroupExpandListener(this)
+        mList!!.setOnGroupCollapseListener(this)
+
+        if (mFinishedStart) {
+            setListAdapter(mAdapter)
+        }
+        mFinishedStart = true
+    }
+
+    /**
+     * Provide the adapter for the expandable list.
+     */
+    private fun setListAdapter(adapter: ExpandableListAdapter?) {
+        synchronized(this) {
+            ensureList()
+            mAdapter = adapter
+            mList!!.setAdapter(adapter)
+        }
+    }
+
+    private fun ensureList() {
+        if (mList != null) {
+            return
+        }
+        setContentView(android.R.layout.expandable_list_content)
+    }
+
 }
